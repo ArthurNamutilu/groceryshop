d3e79f78d37e91f6cfcd0bd91054c30480673e5a
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-20 14:24:26 +0300

    press buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
+// import 'package:flutter/material.dart';
+// import 'package:groceryapp/model/cart_model.dart';
+// import 'package:provider/provider.dart';
+// import 'pages/intro_screen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // using imported provider
+//     return ChangeNotifierProvider(
+//       create: (context) => CartModel(),
+//       child: const MaterialApp(
+//         //debug banner
+//         debugShowCheckedModeBanner: false,
+//         home: IntroScreen(),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:groceryapp/model/cart_model.dart';
-import 'package:provider/provider.dart';
-import 'pages/intro_screen.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +36,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // using imported provider
-    return ChangeNotifierProvider(
-      create: (context) => CartModel(),
-      child: const MaterialApp(
-        //debug banner
-        debugShowCheckedModeBanner: false,
-        home: IntroScreen(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
