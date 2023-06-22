fa6446bc92edf1761255854a90a983f46b061d6c
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-22 12:03:01 +0300

    before loading circle

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../components/my_button.dart';
 import '../components/square_tile.dart';
@@ -8,11 +9,16 @@ class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
   // text editing controllers
-  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   // sign user in method
-  void signUserIn() {}
+  void signUserIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +33,16 @@ class LoginPage extends StatelessWidget {
 
               // logo
               const Icon(
-                Icons.lock,
+                Icons.local_grocery_store,
+//                Icons.lock,
                 size: 100,
               ),
 
               const SizedBox(height: 50),
 
-              // welcome back, you've been missed!
+              // welcome back, message
               Text(
-                'Welcome back you\'ve been missed!',
+                'Welcome sign in to shop fresh items!',
                 style: TextStyle(
                   color: Colors.grey[700],
                   fontSize: 16,
@@ -46,8 +53,8 @@ class LoginPage extends StatelessWidget {
 
               // username textfield
               MyTextField(
-                controller: usernameController,
-                hintText: 'Username',
+                controller: emailController,
+                hintText: 'email',
                 obscureText: false,
               ),
 
@@ -85,51 +92,51 @@ class LoginPage extends StatelessWidget {
 
               const SizedBox(height: 50),
 
-              // or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              // // or continue with
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              //   child: Row(
+              //     children: [
+              //       Expanded(
+              //         child: Divider(
+              //           thickness: 0.5,
+              //           color: Colors.grey[400],
+              //         ),
+              //       ),
+              //       Padding(
+              //         padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              //         child: Text(
+              //           'Or continue with',
+              //           style: TextStyle(color: Colors.grey[700]),
+              //         ),
+              //       ),
+              //       Expanded(
+              //         child: Divider(
+              //           thickness: 0.5,
+              //           color: Colors.grey[400],
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
 
-              const SizedBox(height: 50),
+              // const SizedBox(height: 50),
 
-              // google + apple sign in buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  // google button
-                  SquareTile(imagePath: 'lib/images/google.png'),
+              // // google + apple sign in buttons
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: const [
+              //     // google button
+              //     SquareTile(imagePath: 'lib/images/google.png'),
 
-                  SizedBox(width: 25),
+              //     SizedBox(width: 25),
 
-                  // apple button
-                  SquareTile(imagePath: 'lib/images/apple.png')
-                ],
-              ),
+              //     // apple button
+              //     SquareTile(imagePath: 'lib/images/apple.png')
+              //   ],
+              // ),
 
-              const SizedBox(height: 50),
+              // const SizedBox(height: 50),
 
               // not a member? register now
               Row(
